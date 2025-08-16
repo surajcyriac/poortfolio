ff124f309f2177355251b071683c108959fc8b3f
Suraj Cyriac <surajcyriac@gmail.com>
2025-08-16 16:35:22 +0530

    iddurs

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-// Only polyfill in the browser
-if (typeof window !== 'undefined' && !window.crypto) {
-  import('@peculiar/webcrypto').then(({ Crypto }) => {
-    window.crypto = new Crypto();
-  });
+// import { Buffer } from 'buffer';
+// window.Buffer = Buffer;
+import { Buffer } from 'buffer';
+if (!window.Buffer) {
+  window.Buffer = Buffer;
 }
+
+
+// // Only polyfill in the browser
+// if (typeof window !== 'undefined' && !window.crypto) {
+//   import('@peculiar/webcrypto').then(({ Crypto }) => {
+//     window.crypto = new Crypto();
+//   });
+// }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-
 export default defineConfig({
-  plugins: [react(),    tailwindcss(),
-],
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis'
-      }
-    }
-  }
-});
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
+  },
+  define: {
+    'process.env': {}, // avoid "process is not defined" errors
+  },
+})
